c08e65d591b4618957bc812d2124c0f17d2bbf93
gnothyself <seanpatrick0x@gmail.com>
2021-10-24 09:36:23 -0700

    updated all reservation service handlers

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,5 +1,13 @@
 const knex = require("../db/connection");
 
+// Posts new reservations
+function create(reservation) {
+    return knex("reservations")
+      .insert(reservation)
+      .returning("*")
+      .then((result) => result[0]);
+};
+
 // List all reservations by date, ordered by time
 function list(date) {
     return knex("reservations")
@@ -8,16 +16,37 @@ function list(date) {
         .orderBy("reservation_time");
 };
 
-// Posts new reservations
-function create(reservation) {
+// Returns all reservations with partial match to specified phone number, ordered by date
+function searchByPhone(mobile_number) {
     return knex("reservations")
-      .insert(reservation)
-      .returning("*")
-      .then((result) => result[0]);
-  }
+      .whereRaw(
+        "translate(mobile_number, '() -', '') like ?",
+        `%${mobile_number.replace(/\D/g, "")}%`
+      )
+      .orderBy("reservation_date");
+};
 
+// Returns a reservation by reservation_id
+function read(reservationId) {
+    return knex("reservations")
+        .select("*")
+        .where({ reservation_id: reservationId })
+        .then((returnedRecords) => returnedRecords[0]);
+};
+
+// Updates a reservation status
+function update(updatedReservation) {
+    return knex("reservations")
+        .select("*")
+        .where({ reservation_id: updatedReservation.reservation_id })
+        .update(updatedReservation, "*")
+        .then((updatedReservations) => updatedReservations[0]);
+};
 
 module.exports = {
-    list,
-    create,
+   create,
+   list,
+   searchByPhone,
+   read,
+   update,
 }
